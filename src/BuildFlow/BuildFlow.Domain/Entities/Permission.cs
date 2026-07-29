6073932b843a2ac954a81ddb2c0a2c4ad6a2db70
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-29 23:39:21 +0600

    Add Role Permission Class

--- src/BuildFlow/BuildFlow.Domain/Entities/Permission.cs
+++ src/BuildFlow/BuildFlow.Domain/Entities/Permission.cs
@@ -9,5 +9,5 @@ public class Permission : BaseModel
 {
     public string Name { get; set; } = string.Empty;
 
-    public string Description { get; set; }
+    public string Description { get; set; } = string.Empty;
 }
---
+++ src/BuildFlow/BuildFlow.Domain/Entities/RolePermission.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Domain.Entities;
+
+public class RolePermission
+{
+    public Guid RoleId { get; set; }
+
+    public Guid PermissionId { get; set; }
+}
