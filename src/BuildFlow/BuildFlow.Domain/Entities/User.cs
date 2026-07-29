654492ceee384e38fb4f2a62dfae8f0029c6e7af
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-29 21:44:17 +0600

    Add User Class

---
+++ src/BuildFlow/BuildFlow.Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using BuildFlow.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Domain.Entities;
+
+public class User : BaseModel
+{
+    public Guid TenantId { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string PasswordHash { get; set; } = string.Empty;
+
+    public bool IsActive { get; set; } = true;
+}
