719582da34e42f77249e7def821ae37f0ebb76f4
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-29 20:58:32 +0600

    Add Base Model

--- src/BuildFlow/BuildFlow.Domain/Common/BaseModel.cs
+++ src/BuildFlow/BuildFlow.Domain/Common/BaseModel.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace BuildFlow.Domain.Common
+namespace BuildFlow.Domain.Common;
+
+public class BaseModel
 {
-    internal class BaseModel
-    {
-    }
+    public Guid Id { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public string? CreatedBy { get; set; }
+
+    public DateTime? ModifiedAt { get; set; }
+
+    public string? ModifiedBy { get; set; }
+
+    public bool IsDeleted { get; set; } = false;
 }
