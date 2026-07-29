ef38bd44534e1d17bc63b069cc99439a7eed0ced
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-29 20:56:14 +0600

    Add Domain simple architecture

---
+++ src/BuildFlow/BuildFlow.Domain/Entities/Tenant.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Domain.Entities
+{
+    internal class Tenant
+    {
+    }
+}
---
+++ src/BuildFlow/BuildFlow.Domain/Enums/TenantStatus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Domain.Enums
+{
+    internal class TenantStatus
+    {
+    }
+}
