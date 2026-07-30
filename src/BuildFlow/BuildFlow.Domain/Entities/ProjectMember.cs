8809c4aaa4ddb2a01aaaaebcb24303cfa127d530
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-30 10:51:49 +0600

    Add project member

---
+++ src/BuildFlow/BuildFlow.Domain/Entities/ProjectMember.cs
@@ -0,0 +1,18 @@
+﻿using BuildFlow.Domain.Common;
+using BuildFlow.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Domain.Entities;
+
+public class ProjectMember : BaseModel
+{
+    public Guid ProjectId { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public ProjectMemberRole Role { get; set; }
+
+    public DateTime JoinedDate { get; set; } = DateTime.UtcNow;
+}
