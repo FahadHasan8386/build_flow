80a459c872c54048cd98f0d562d105660010eb44
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-30 13:49:43 +0600

    Add RegisterTenantResponse for Register Tenant User

---
+++ src/BuildFlow/BuildFlow.Application/Features/Identity/RegisterTenant/Responses/RegisterTenantResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Features.Identity.RegisterTenant.Responses;
+
+public class RegisterTenantResponse
+{
+    public bool Success { get; set; }
+
+    public string Message { get; set; } = string.Empty;
+
+    public Guid TenantId { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public string AccessToken { get; set; } = string.Empty;
+
+    public string RefreshToken { get; set; } = string.Empty;
+}
