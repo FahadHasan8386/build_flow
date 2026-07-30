41c1e831f1a4599ba3e8d6db76b80c54a11b4ebf
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-30 15:37:03 +0600

    Add Application file structure for identity Features

---
+++ src/BuildFlow/BuildFlow.Application/Features/Identity/Login/LoginRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Features.Identity.Login
+{
+    internal class LoginRequest
+    {
+    }
+}
---
+++ src/BuildFlow/BuildFlow.Application/Features/Identity/RegisterTenant/RegisterTenantRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Features.Identity.RegisterTenant
+{
+    internal class RegisterTenantRequest
+    {
+    }
+}
