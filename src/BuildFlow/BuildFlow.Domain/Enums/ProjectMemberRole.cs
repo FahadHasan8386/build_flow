97841de4e8521f90d848b19748f5cb4ad70187ab
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-30 10:49:53 +0600

    Add  ProjectMemberRole for better experience

---
+++ src/BuildFlow/BuildFlow.Domain/Enums/ProjectMemberRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Domain.Enums;
+
+public enum ProjectMemberRole
+{
+    ProjectManager = 1,
+    Engineer = 2, 
+    SiteSupervisor = 3,
+    Accountant = 4,
+    QualityEngineer = 5,
+    Viewer = 6
+}
