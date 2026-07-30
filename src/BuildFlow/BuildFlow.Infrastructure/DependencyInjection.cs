08f76615cf58d8fc584d2ea52f3cec1582a61d28
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-30 20:29:21 +0600

    Add Password  Hasher

--- src/BuildFlow/BuildFlow.Infrastructure/DependencyInjection.cs
+++ src/BuildFlow/BuildFlow.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using BuildFlow.Infrastructure.Persistence;
+﻿using BuildFlow.Application.Interfaces.Security;
+using BuildFlow.Infrastructure.Persistence;
+using BuildFlow.Infrastructure.Security;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -11,7 +13,7 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
         services.AddScoped<DbConnectionFactory>();
-
+        services.AddScoped<IPasswordHasher, PasswordHasher>();
         return services;
     }
 }
--- src/BuildFlow/BuildFlow.Infrastructure/Security/PasswordHasher.cs
+++ src/BuildFlow/BuildFlow.Infrastructure/Security/PasswordHasher.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using BuildFlow.Application.Interfaces.Security;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace BuildFlow.Infrastructure.Security
+namespace BuildFlow.Infrastructure.Security;
+
+public class PasswordHasher : IPasswordHasher
 {
-    internal class PasswordHasher
+    public string HashPassword(string password)
     {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool VerifyPassword(string password, string passwordHash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, passwordHash);
     }
 }
