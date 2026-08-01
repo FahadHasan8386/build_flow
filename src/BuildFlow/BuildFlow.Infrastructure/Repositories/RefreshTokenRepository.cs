35ebced65760dad571de08a8e208db0486fe6e51
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-01 14:00:08 +0600

    Add Refresh Token Repository Query

--- src/BuildFlow/BuildFlow.Infrastructure/Repositories/RefreshTokenRepository.cs
+++ src/BuildFlow/BuildFlow.Infrastructure/Repositories/RefreshTokenRepository.cs
@@ -1,10 +1,52 @@
-﻿using System;
+﻿using BuildFlow.Application.Interfaces.Repositories;
+using BuildFlow.Domain.Entities;
+using BuildFlow.Infrastructure.Persistence;
+using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace BuildFlow.Infrastructure.Repositories
+namespace BuildFlow.Infrastructure.Repositories;
+
+public class RefreshTokenRepository : IRefreshTokenRepository
 {
-    internal class RefreshTokenRepository
+    private readonly DbConnectionFactory _connectionFactory;
+
+    public RefreshTokenRepository(DbConnectionFactory connectionFactory)
     {
+        _connectionFactory = connectionFactory;
+    }
+
+    public async Task CreateAsync(RefreshToken refreshToken)
+    {
+        using var connection = _connectionFactory.CreateConnection();
+
+        const string sql = @"
+                            INSERT INTO RefreshTokens
+                            (Id, UserId,Token, ExpiresAt, RevokedAt,CreatedBy,CreatedAt,ModifiedBy,ModifiedAt,InActive)
+                            VALUES
+                            (@Id,@UserId,@Token,@ExpiresAt,@RevokedAt,@CreatedBy,@CreatedAt,@ModifiedBy,@ModifiedAt,@InActive);";
+
+        await connection.ExecuteAsync(sql, refreshToken);
+    }
+
+    public async Task<RefreshToken?> GetByTokenAsync(string token)
+    {
+        using var connection = _connectionFactory.CreateConnection();
+
+        return await connection.QueryFirstOrDefaultAsync<RefreshToken>(
+            "SELECT * FROM RefreshTokens WHERE Token=@Token",
+            new { Token = token });
+    }
+
+    public async Task RevokeAsync(string token)
+    {
+        using var connection = _connectionFactory.CreateConnection();
+
+        await connection.ExecuteAsync(
+            @"UPDATE RefreshTokens
+              SET RevokedAt = GETUTCDATE()
+              WHERE Token=@Token",
+            new { Token = token });
     }
 }
