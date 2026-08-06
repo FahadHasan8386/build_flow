0ae87640ad8453a29890db5d9e44d4334808b99d
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-06 21:26:21 +0600

    Add Project Create Project Response

--- src/BuildFlow/BuildFlow.Application/Features/Projects/CreateProject/CreateProjectResponse.cs
+++ src/BuildFlow/BuildFlow.Application/Features/Projects/CreateProject/CreateProjectResponse.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace BuildFlow.Application.Features.Projects.CreateProject
+namespace BuildFlow.Application.Features.Projects.CreateProject;
+
+public class CreateProjectResponse
 {
-    internal class CreateProjectResponse
-    {
-    }
+    public bool Success { get; set; }
+
+    public string Message { get; set; } = string.Empty;
+
+    public Guid ProjectId { get; set; }
 }
