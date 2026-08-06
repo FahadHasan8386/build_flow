f09f553d23a747f8351cd2597d01472992ee267b
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-06 21:33:42 +0600

    Add Project Entity

---
+++ src/BuildFlow/BuildFlow.Domain/Entities/Project.cs
@@ -0,0 +1,24 @@
+﻿using BuildFlow.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Domain.Entities;
+
+public class Project : BaseModel
+{
+    public Guid TenantId { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public DateTime? StartDate { get; set; }
+
+    public DateTime? EndDate { get; set; }
+
+    public bool IsArchived { get; set; } = false;
+
+    public Guid CreatedByUserId { get; set; }
+
+}
