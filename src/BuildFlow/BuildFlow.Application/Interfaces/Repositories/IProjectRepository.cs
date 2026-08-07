460b0006a63049895857c72d41bdeca3e8f45bff
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-07 16:36:02 +0600

    Add Interfaces For Project Repository

---
+++ src/BuildFlow/BuildFlow.Application/Interfaces/Repositories/IProjectRepository.cs
@@ -0,0 +1,16 @@
+﻿using BuildFlow.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace BuildFlow.Application.Interfaces.Repositories;
+
+public interface IProjectRepository
+{
+    Task<Guid> CreateAsync(Project project, IDbConnection connection , IDbTransaction transaction );
+    Task<Project?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Project>> GetByTenantAsync(Guid tenantId);
+    Task UpdateAsync(Project project);
+    Task DeleteAsync(Guid id);
+}
