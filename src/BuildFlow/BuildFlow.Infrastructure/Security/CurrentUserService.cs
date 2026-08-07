b0b058dce15d337e9249cccbce9712990db3ef21
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-07 20:51:24 +0600

    Current User Service to identify the user

---
+++ src/BuildFlow/BuildFlow.Infrastructure/Security/CurrentUserService.cs
@@ -0,0 +1,38 @@
+﻿using BuildFlow.Application.Interfaces.Security;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace BuildFlow.Infrastructure.Security;
+
+public class CurrentUserService : ICurrentUserService
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public bool IsAuthenticated => _httpContextAccessor.HttpContext?.User?.Identity?.IsAuthenticated ?? false;
+
+    public Guid UserId =>
+        GetGuidClaim(ClaimTypes.NameIdentifier);
+
+    public Guid TenantId =>
+        GetGuidClaim("TenantId");
+
+    private Guid GetGuidClaim(string claimType)
+    {
+        var value = _httpContextAccessor.HttpContext?
+            .User?
+            .FindFirst(claimType)?
+            .Value;
+
+        return Guid.TryParse(value, out var id)
+            ? id
+            : Guid.Empty;
+    }
+}
