88ff764d153205b418ee8e75b5649df21f6c80d1
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-10 20:56:09 +0600

    Add Update request and commad File

--- src/BuildFlow/BuildFlow.Application/Features/Projects/UpdateProject/UpdateProjectCommand.cs
+++ src/BuildFlow/BuildFlow.Application/Features/Projects/UpdateProject/UpdateProjectCommand.cs
@@ -1,10 +1,8 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace BuildFlow.Application.Features.Projects.UpdateProject
-{
-    internal class UpdateProjectCommand
-    {
-    }
-}
+namespace BuildFlow.Application.Features.Projects.UpdateProject;
+
+public record UpdateProjectCommand(Guid ProjectId,UpdateProjectRequest Request) : IRequest<UpdateProjectResponse>;
--- src/BuildFlow/BuildFlow.Application/Features/Projects/UpdateProject/UpdateProjectRequest.cs
+++ src/BuildFlow/BuildFlow.Application/Features/Projects/UpdateProject/UpdateProjectRequest.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace BuildFlow.Application.Features.Projects.UpdateProject
+namespace BuildFlow.Application.Features.Projects.UpdateProject;
+
+public class UpdateProjectRequest
 {
-    internal class UpdateProjectRequest
-    {
-    }
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public DateTime? StartDate { get; set; }
+
+    public DateTime? EndDate { get; set; }
+
+    public bool IsArchived { get; set; }
 }
