207f748bd906f2717a1b7b72ff92d1c575d87fc8
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-10 11:57:23 +0600

    Project Data  for Project Response

---
+++ src/BuildFlow/BuildFlow.Application/Features/Projects/GetProjectById/ProjectData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Features.Projects.GetProjectById;
+
+public class ProjectData
+{
+    public Guid Id { get; set; }
+
+    public Guid TenantId { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public DateTime? StartDate { get; set; }
+
+    public DateTime? EndDate { get; set; }
+
+    public bool IsArchived { get; set; }
+}
