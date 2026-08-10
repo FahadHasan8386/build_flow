e8f4b724adc384564fdaf36476736fc1dca82d73
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-10 21:05:04 +0600

    Add Update validator File

--- src/BuildFlow/BuildFlow.Application/Features/Projects/UpdateProject/UpdateProjectValidator.cs
+++ src/BuildFlow/BuildFlow.Application/Features/Projects/UpdateProject/UpdateProjectValidator.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace BuildFlow.Application.Features.Projects.UpdateProject
+namespace BuildFlow.Application.Features.Projects.UpdateProject;
+
+public class UpdateProjectValidator : AbstractValidator<UpdateProjectCommand>
 {
-    internal class UpdateProjectValidator
-    {
-    }
+     public UpdateProjectValidator()
+     {
+        RuleFor(x => x.ProjectId)
+            .NotEmpty()
+            .WithMessage("Project ID is required.");
+
+        RuleFor(x => x.Request.Name)
+            .NotEmpty()
+            .WithMessage("Project name is required.")
+            .MaximumLength(200)
+            .WithMessage("Project name cannot exceed 200 characters.");
+
+        RuleFor(x => x.Request.Description)
+            .MaximumLength(1000)
+            .WithMessage("Description cannot exceed 1000 characters.");
+
+        RuleFor(x => x.Request)
+            .Must(x =>
+                !x.StartDate.HasValue ||
+                !x.EndDate.HasValue ||
+                x.StartDate <= x.EndDate)
+            .WithMessage("Start date must be before or equal to end date.");
+     }
 }
+
