43e59a829f56a7be0408cca8b48b15f9a3161354
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-10 13:20:50 +0600

    Add Get All Project for specific tenanat user

--- src/BuildFlow/BuildFlow.Application/Features/Projects/GetProjects/GetProjectsQuery.cs
+++ src/BuildFlow/BuildFlow.Application/Features/Projects/GetProjects/GetProjectsQuery.cs
@@ -1,10 +1,8 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace BuildFlow.Application.Features.Projects.GetProjects
-{
-    internal class GetProjectsQuery
-    {
-    }
-}
+namespace BuildFlow.Application.Features.Projects.GetProjects;
+
+public record GetProjectsQuery : IRequest<GetProjectsResponse>;
--- src/BuildFlow/BuildFlow.Application/Features/Projects/GetProjects/GetProjectsResponse.cs
+++ src/BuildFlow/BuildFlow.Application/Features/Projects/GetProjects/GetProjectsResponse.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace BuildFlow.Application.Features.Projects.GetProjects
+namespace BuildFlow.Application.Features.Projects.GetProjects;
+
+public class GetProjectsResponse
 {
-    internal class GetProjectsResponse
-    {
-    }
+    public bool Success { get; set; }
+
+    public string Message { get; set; } = string.Empty;
+
+    public IEnumerable<ProjectData> Projects { get; set; }= new List<ProjectData>();
 }
