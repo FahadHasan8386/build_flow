5353fef24a7f8936bfd9c59ef80b3f1657eada4c
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-11 15:33:39 +0600

    Add project Controller

---
+++ src/BuildFlow/BuildFlow.api/Controllers/ProjectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BuildFlow.api.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
