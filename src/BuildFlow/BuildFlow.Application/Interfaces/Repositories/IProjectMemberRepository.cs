e847cdbfed730a01357c7e1fb6a478fb64c38779
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-17 21:21:15 +0600

    Add Project Member Repository Interfaces

---
+++ src/BuildFlow/BuildFlow.Application/Interfaces/Repositories/IProjectMemberRepository.cs
@@ -0,0 +1,24 @@
+﻿using BuildFlow.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Interfaces.Repositories
+{
+    public interface IProjectMemberRepository
+    {
+        Task<Guid> AddMemberAsync(ProjectMember member);
+
+        Task<ProjectMember?> GetMemberAsync(Guid projectId,Guid userId,Guid tenantId);
+
+        Task<IEnumerable<ProjectMember>> GetMembersByProjectAsync(Guid projectId,Guid tenantId);
+
+        Task RemoveMemberAsync(Guid projectId,Guid userId,Guid tenantId);
+
+        Task AddRoleAsync(ProjectMemberRole role);
+
+        Task RemoveRoleAsync(Guid projectMemberId,int role);
+
+        Task<IEnumerable<ProjectMemberRole>> GetRolesAsync(Guid projectMemberId);
+    }
+}
