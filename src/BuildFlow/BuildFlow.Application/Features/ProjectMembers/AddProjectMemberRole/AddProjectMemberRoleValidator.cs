dde8bfe83443f5b79179d15595feaa6eafbb4480
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-21 16:49:31 +0600

    Add Project Member Role Validator

---
+++ src/BuildFlow/BuildFlow.Application/Features/ProjectMembers/AddProjectMemberRole/AddProjectMemberRoleValidator.cs
@@ -0,0 +1,16 @@
+﻿using BuildFlow.Domain.Enums;
+using FluentValidation;
+
+namespace BuildFlow.Application.Features.ProjectMembers.AddProjectMemberRole;
+
+public class AddProjectMemberRoleValidator : AbstractValidator<AddProjectMemberRoleRequest>
+{
+    public AddProjectMemberRoleValidator()
+    {
+        RuleFor(x => x.Role)
+            .Must(role => Enum.IsDefined(
+                typeof(ProjectMemberRoleType),
+                role))
+            .WithMessage("Invalid project member role.");
+    }
+}
