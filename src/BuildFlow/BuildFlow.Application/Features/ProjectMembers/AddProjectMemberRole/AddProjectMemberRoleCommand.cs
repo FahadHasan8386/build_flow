9faa532b974b7845dd30e05b9a40c85cefd1e72f
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-21 16:46:59 +0600

    Add Project Member Role Command

---
+++ src/BuildFlow/BuildFlow.Application/Features/ProjectMembers/AddProjectMemberRole/AddProjectMemberRoleCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Features.ProjectMembers.AddProjectMemberRole;
+
+public record AddProjectMemberRoleCommand(
+    Guid ProjectId,
+    Guid UserId, AddProjectMemberRoleRequest Request) : IRequest<AddProjectMemberRoleResponse>;
