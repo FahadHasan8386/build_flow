0d3c90d15046546a94eaa4b6b80cb6e9294d7d22
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-22 15:51:05 +0600

    Remove Project Member Role validation

---
+++ src/BuildFlow/BuildFlow.Application/Features/ProjectMembers/RemoveProjectMemberRole/RemoveProjectMemberRoleValidator.cs
@@ -0,0 +1,20 @@
+﻿using BuildFlow.Domain.Enums;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Features.ProjectMembers.RemoveProjectMemberRole;
+
+public class RemoveProjectMemberRoleValidator
+    : AbstractValidator<RemoveProjectMemberRoleRequest>
+{
+    public RemoveProjectMemberRoleValidator()
+    {
+        RuleFor(x => x.Role)
+            .Must(role => Enum.IsDefined(
+                typeof(ProjectMemberRoleType),
+                role))
+            .WithMessage("Invalid project member role.");
+    }
+}
