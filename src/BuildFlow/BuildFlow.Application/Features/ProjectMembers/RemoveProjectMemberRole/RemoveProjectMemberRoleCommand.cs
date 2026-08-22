19e434121c299267835bd584ef8973b1e9c4b7f9
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-22 15:47:21 +0600

    Add Remove Project Member Role Response , Command

---
+++ src/BuildFlow/BuildFlow.Application/Features/ProjectMembers/RemoveProjectMemberRole/RemoveProjectMemberRoleCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Features.ProjectMembers.RemoveProjectMemberRole;
+
+public record RemoveProjectMemberRoleCommand(Guid ProjectId,Guid UserId,
+RemoveProjectMemberRoleRequest Request) : IRequest<RemoveProjectMemberRoleResponse>;
---
+++ src/BuildFlow/BuildFlow.Application/Features/ProjectMembers/RemoveProjectMemberRole/RemoveProjectMemberRoleResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Features.ProjectMembers.RemoveProjectMemberRole;
+
+public class RemoveProjectMemberRoleResponse
+{
+    public bool Success { get; set; }
+
+    public string Message { get; set; } = string.Empty;
+}
