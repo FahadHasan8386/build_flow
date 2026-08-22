c85a6f33ccc79bbd3c56c884db123cfc753d82e1
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-22 15:55:12 +0600

    Remove Project Member Command, Response

---
+++ src/BuildFlow/BuildFlow.Application/Features/ProjectMembers/RemoveProjectMember/RemoveProjectMemberCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Features.ProjectMembers.RemoveProjectMember;
+
+public record RemoveProjectMemberCommand(Guid ProjectId,Guid UserId) : IRequest<RemoveProjectMemberResponse>;
---
+++ src/BuildFlow/BuildFlow.Application/Features/ProjectMembers/RemoveProjectMember/RemoveProjectMemberResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Features.ProjectMembers.RemoveProjectMember;
+
+public class RemoveProjectMemberResponse
+{
+    public bool Success { get; set; }
+
+    public string Message { get; set; } = string.Empty;
+}
