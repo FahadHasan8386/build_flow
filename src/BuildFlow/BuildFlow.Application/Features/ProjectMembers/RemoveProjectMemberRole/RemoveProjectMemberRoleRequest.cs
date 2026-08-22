d8e8dc4d851e08b626f5918cfd01d797caea6820
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-22 15:45:19 +0600

    Add Remove Project Member Role Request

---
+++ src/BuildFlow/BuildFlow.Application/Features/ProjectMembers/RemoveProjectMemberRole/RemoveProjectMemberRoleRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Features.ProjectMembers.RemoveProjectMemberRole;
+
+public class RemoveProjectMemberRoleRequest
+{
+    public int Role { get; set; }
+}
