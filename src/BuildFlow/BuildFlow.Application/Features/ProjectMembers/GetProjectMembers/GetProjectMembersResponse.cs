7643a63ec2dd746edd996d0ae9a463c483cad9c3
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-25 10:12:07 +0600

    Api Response Create project , get project member response

--- src/BuildFlow/BuildFlow.Application/Features/ProjectMembers/GetProjectMembers/GetProjectMembersResponse.cs
+++ src/BuildFlow/BuildFlow.Application/Features/ProjectMembers/GetProjectMembers/GetProjectMembersResponse.cs
@@ -1,15 +1,12 @@
-﻿using System;
+﻿using BuildFlow.Shared.Responses;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BuildFlow.Application.Features.ProjectMembers.GetProjectMembers;
 
-public class GetProjectMembersResponse
+public class GetProjectMembersResponse : ApiResponse
 {
-    public bool Success { get; set; }
-
-    public string Message { get; set; } = string.Empty;
-
     public IEnumerable<ProjectMemberItem> Members { get; set; }
         = new List<ProjectMemberItem>();
 }
--- src/BuildFlow/BuildFlow.Application/Features/Projects/CreateProject/CreateProjectResponse.cs
+++ src/BuildFlow/BuildFlow.Application/Features/Projects/CreateProject/CreateProjectResponse.cs
@@ -1,14 +1,11 @@
-﻿using System;
+﻿using BuildFlow.Shared.Responses;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BuildFlow.Application.Features.Projects.CreateProject;
 
-public class CreateProjectResponse
+public class CreateProjectResponse : ApiResponse
 {
-    public bool Success { get; set; }
-
-    public string Message { get; set; } = string.Empty;
-
     public Guid ProjectId { get; set; }
 }
