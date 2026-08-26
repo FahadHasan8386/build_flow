2d2b1fe5b3fa4820be543dc181543b97dd9a0902
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-26 11:43:09 +0600

    Add Project Member Role Shared ApiResponse

--- src/BuildFlow/BuildFlow.Application/Features/ProjectMembers/AddProjectMemberRole/AddProjectMemberRoleResponse.cs
+++ src/BuildFlow/BuildFlow.Application/Features/ProjectMembers/AddProjectMemberRole/AddProjectMemberRoleResponse.cs
@@ -1,12 +1,10 @@
-﻿using System;
+﻿using BuildFlow.Shared.Responses;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BuildFlow.Application.Features.ProjectMembers.AddProjectMemberRole;
 
-public class AddProjectMemberRoleResponse
+public class AddProjectMemberRoleResponse : ApiResponse
 {
-    public bool Success { get; set; }
-
-    public string Message { get; set; } = string.Empty;
 }
