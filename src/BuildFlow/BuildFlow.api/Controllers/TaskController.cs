25d1935580ba787569be8c7d966d62297de41659
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-28 17:40:01 +0600

    Create Task item Controller

---
+++ src/BuildFlow/BuildFlow.api/Controllers/TaskController.cs
@@ -0,0 +1,35 @@
+﻿using BuildFlow.Application.Features.Tasks.CreateTask;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildFlow.api.Controllers;
+
+[ApiController]
+[Route("api/tasks")]
+[Authorize]
+public class TaskController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public TaskController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(
+        [FromBody] CreateTaskRequest request)
+    {
+        var command = new CreateTaskCommand(request);
+
+        var result = await _mediator.Send(command);
+
+        if (!result.Success)
+        {
+            return BadRequest(result);
+        }
+
+        return Ok(result);
+    }
+}
