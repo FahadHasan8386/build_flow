edbe3b683d3dd9e1a85f810e4a1e1ee419e9081d
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-28 16:41:14 +0600

    Add Task Item Entity

---
+++ src/BuildFlow/BuildFlow.Domain/Entities/TaskItem.cs
@@ -0,0 +1,25 @@
+﻿using BuildFlow.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Domain.Entities;
+
+public class TaskItem : BaseModel
+{
+    public Guid TenantId { get; set; }
+
+    public Guid ProjectId { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public TaskStatus Status { get; set; } = TaskStatus.Todo;
+
+    public TaskPriority Priority { get; set; } = TaskPriority.Medium;
+
+    public Guid? AssignedToUserId { get; set; }
+
+    public DateTime? DueDate { get; set; }
+}
