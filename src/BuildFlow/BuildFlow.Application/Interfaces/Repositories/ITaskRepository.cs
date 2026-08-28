b5a1c1f31e1126b6888d28274ce5d5bcfe4ecf14
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-28 16:52:24 +0600

    Create Interface repository for taskItem

--- src/BuildFlow/BuildFlow.Application/Interfaces/Repositories/ITaskRepository.cs
+++ src/BuildFlow/BuildFlow.Application/Interfaces/Repositories/ITaskRepository.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using BuildFlow.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BuildFlow.Application.Interfaces.Repositories
 {
-    internal interface ITaskRepository
+    public interface ITaskRepository
     {
+        Task<TaskItem?> GetByIdAsync(Guid taskId,Guid tenantId);
+
+        Task<IEnumerable<TaskItem>> GetByProjectAsync(Guid projectId,Guid tenantId);
+
+        Task AddAsync(TaskItem task);
+
+        Task UpdateAsync(TaskItem task);
+
+        Task DeleteAsync(Guid taskId,Guid tenantId);
     }
 }
