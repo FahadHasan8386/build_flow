ac657329f5d0dc23c927026c8b034bc963b9caa2
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-28 17:35:49 +0600

    Create Task item Fluent Validation

--- src/BuildFlow/BuildFlow.Application/Features/Tasks/CreateTask/CreateTaskValidator.cs
+++ src/BuildFlow/BuildFlow.Application/Features/Tasks/CreateTask/CreateTaskValidator.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace BuildFlow.Application.Features.Tasks.CreateTask
+namespace BuildFlow.Application.Features.Tasks.CreateTask;
+
+public class CreateTaskValidator : AbstractValidator<CreateTaskCommand>
 {
-    internal class CreateTaskValidator
+    public CreateTaskValidator()
     {
+        RuleFor(x => x.Request.ProjectId)
+            .NotEmpty()
+            .WithMessage("Project ID is required.");
+
+        RuleFor(x => x.Request.Title)
+            .NotEmpty()
+            .WithMessage("Task title is required.")
+            .MaximumLength(200)
+            .WithMessage("Task title cannot exceed 200 characters.");
+
+        RuleFor(x => x.Request.Description)
+            .MaximumLength(2000)
+            .WithMessage("Task description cannot exceed 2000 characters.");
+
+        RuleFor(x => x.Request.DueDate)
+            .GreaterThan(DateTime.UtcNow)
+            .When(x => x.Request.DueDate.HasValue)
+            .WithMessage("Due date must be in the future.");
     }
 }
