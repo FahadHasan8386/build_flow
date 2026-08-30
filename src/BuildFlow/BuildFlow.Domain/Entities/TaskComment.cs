9138568bdede341d0f4db1e24d1fba125922fab1
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-30 16:47:56 +0600

    AdD entity Task Commnet

---
+++ src/BuildFlow/BuildFlow.Domain/Entities/TaskComment.cs
@@ -0,0 +1,18 @@
+﻿using BuildFlow.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Domain.Entities
+{
+    public class TaskComment : BaseModel
+    {
+        public Guid TenantId { get; set; }
+
+        public Guid TaskId { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public string Comment { get; set; } = string.Empty;
+    }
+}
