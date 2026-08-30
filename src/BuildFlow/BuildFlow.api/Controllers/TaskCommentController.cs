6eeee18656140458aa69256e6e0d21a4263ffc7c
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-30 21:44:43 +0600

    Add a new feature Add Comment Controller

---
+++ src/BuildFlow/BuildFlow.api/Controllers/TaskCommentController.cs
@@ -0,0 +1,35 @@
+﻿using BuildFlow.Application.Features.TaskComments.AddComments;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildFlow.api.Controllers;
+
+[ApiController]
+[Route("api/task-comments")]
+[Authorize]
+public class TaskCommentController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public TaskCommentController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddComment(
+        [FromBody] AddCommentRequest request)
+    {
+        var command = new AddCommentCommand(request);
+
+        var result = await _mediator.Send(command);
+
+        if (!result.Success)
+        {
+            return BadRequest(result);
+        }
+
+        return Ok(result);
+    }
+}
