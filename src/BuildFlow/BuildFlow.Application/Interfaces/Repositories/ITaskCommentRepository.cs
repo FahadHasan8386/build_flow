db7e1b5a28e2a8fccc0836f436ecd200e636b46a
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-30 16:49:45 +0600

    AdD interface for  Task Commnet

---
+++ src/BuildFlow/BuildFlow.Application/Interfaces/Repositories/ITaskCommentRepository.cs
@@ -0,0 +1,25 @@
+﻿using BuildFlow.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Interfaces.Repositories;
+
+public interface ITaskCommentRepository
+{
+    Task AddAsync(TaskComment comment);
+
+    Task<IEnumerable<TaskComment>> GetByTaskAsync(
+        Guid taskId,
+        Guid tenantId);
+
+    Task<TaskComment?> GetByIdAsync(
+        Guid commentId,
+        Guid tenantId);
+
+    Task UpdateAsync(TaskComment comment);
+
+    Task DeleteAsync(
+        Guid commentId,
+        Guid tenantId);
+}
