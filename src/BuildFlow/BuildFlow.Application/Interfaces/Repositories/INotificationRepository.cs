e3ce5ffcf4bc25b66a87d995355243429c9dc0f9
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-31 11:13:48 +0600

    Add Notification interface Repository

---
+++ src/BuildFlow/BuildFlow.Application/Interfaces/Repositories/INotificationRepository.cs
@@ -0,0 +1,22 @@
+﻿using BuildFlow.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Interfaces.Repositories;
+
+public interface INotificationRepository
+{
+     Task AddAsync(Notification notification);
+
+     Task<IEnumerable<Notification>> GetByUserAsync(Guid userId , Guid tenantId);
+
+     Task<Notification?> GetByIdAsync(Guid nitificationId , Guid userId , Guid tenantId);
+
+     Task MarkAsReadAsync(Guid notificationId, Guid userId, Guid tenantId);
+
+     Task MarkAllAsReadAsync(Guid userId, Guid tenantId);
+
+     Task DeleteAsync(Guid notificationId, Guid userId, Guid tenantId);
+
+}
