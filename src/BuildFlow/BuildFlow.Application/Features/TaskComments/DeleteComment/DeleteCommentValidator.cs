69902c588db124122448e400b647aa775e4e81a3
Fahad Hasan <hasan.fahad67@outlook.com>
2026-08-31 10:54:10 +0600

    Add Delete comment feature Validation

---
+++ src/BuildFlow/BuildFlow.Application/Features/TaskComments/DeleteComment/DeleteCommentValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Features.TaskComments.DeleteComment;
+
+public class DeleteCommentValidator : AbstractValidator<DeleteCommentCommand>
+{
+    public DeleteCommentValidator()
+    {
+        RuleFor(x => x.CommentId)
+            .NotEmpty()
+            .WithMessage("Comment ID is required.");
+    }
+}
