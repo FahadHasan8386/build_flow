72cfc07f36d323ba5c8f500aa00c462d56959a7d
Fahad Hasan <hasan.fahad67@outlook.com>
2026-09-03 20:18:11 +0600

    Add Notiication Featuer Get By User Notification Handler

--- src/BuildFlow/BuildFlow.Application/Features/Notifications/GetNotifications/GetNotificationsHandler.cs
+++ src/BuildFlow/BuildFlow.Application/Features/Notifications/GetNotifications/GetNotificationsHandler.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using BuildFlow.Application.Interfaces.Repositories;
+using BuildFlow.Application.Interfaces.Security;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace BuildFlow.Application.Features.Notifications.GetNotifications
+namespace BuildFlow.Application.Features.Notifications.GetNotifications;
+
+public class GetNotificationsHandler : IRequestHandler<GetNotificationsQuery, GetNotificationsResponse>
 {
-    internal class GetNotificationsHandler
+    private readonly INotificationRepository _notifiicationRepository;
+    private readonly ICurrentUserService _currentUserService;
+
+    public GetNotificationsHandler(INotificationRepository notifiicationRepository,
+        ICurrentUserService currentUserService)
     {
+        _notifiicationRepository = notifiicationRepository;
+        _currentUserService = currentUserService;
     }
+
+    public async Task<GetNotificationsResponse> Handle(GetNotificationsQuery request, CancellationToken cancellationToken)
+    {
+        var userId = _currentUserService.UserId;
+        var tenantId = _currentUserService.TenantId;
+
+        var notifications = await _notifiicationRepository.GetByUserAsync(userId, tenantId);
+
+
+        return new GetNotificationsResponse
+        {
+            Success = true,
+            Message = "Notifications retrieved successfully.",
+            Notifications = notifications
+        };
+    }
+
 }
