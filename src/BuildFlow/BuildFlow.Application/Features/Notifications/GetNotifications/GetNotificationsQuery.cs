7e751c979df0dbf36867a215cdc39f6ecfe45745
Fahad Hasan <hasan.fahad67@outlook.com>
2026-09-03 20:08:55 +0600

    Add Notiication Featuer

---
+++ src/BuildFlow/BuildFlow.Application/Features/Notifications/GetNotifications/GetNotificationsQuery.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Features.Notifications.GetNotifications
+{
+    internal class GetNotificationsQuery
+    {
+    }
+}
