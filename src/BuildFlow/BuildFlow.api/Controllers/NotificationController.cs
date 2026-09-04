a7b97b1a95f84c82171c48abf8b0e1d2cc54fd70
Fahad Hasan <hasan.fahad67@outlook.com>
2026-09-04 10:46:45 +0600

    Add Notiication Featuer Get By User Notification Controller

---
+++ src/BuildFlow/BuildFlow.api/Controllers/NotificationController.cs
@@ -0,0 +1,30 @@
+﻿using BuildFlow.Application.Features.Notifications.GetNotifications;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildFlow.api.Controllers
+{
+    [ApiController]
+    [Route("api/notifications")]
+    [Authorize]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public NotificationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetNotifications()
+        {
+            var query = new GetNotificationsQuery();
+
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
+        }
+    }
+}
