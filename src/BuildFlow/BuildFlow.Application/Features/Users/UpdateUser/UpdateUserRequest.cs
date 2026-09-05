e1e07b0b7ecc55b0c5f8335b8da07ed6b5e393fe
Fahad Hasan <hasan.fahad67@outlook.com>
2026-09-05 19:54:04 +0600

    AdD Feature users for Update User

---
+++ src/BuildFlow/BuildFlow.Application/Features/Users/UpdateUser/UpdateUserRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Features.Users.UpdateUser;
+
+public class UpdateUserRequest
+{
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+}
---
+++ src/BuildFlow/BuildFlow.Application/Features/Users/UpdateUser/UpdateUserResponse.cs
@@ -0,0 +1,12 @@
+﻿using BuildFlow.Shared.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Features.Users.UpdateUser
+{
+    public class UpdateUserResponse : ApiResponse
+    {
+        public Guid UserId { get; set; }
+    }
+}
