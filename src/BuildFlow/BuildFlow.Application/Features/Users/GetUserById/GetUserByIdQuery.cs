17fe397d7b63c1703b52348dc01d4293b3cf043c
Fahad Hasan <hasan.fahad67@outlook.com>
2026-09-05 16:04:55 +0600

    AdD Feature users for GetBy Id User query , response , userdetails dto

---
+++ src/BuildFlow/BuildFlow.Application/Features/Users/GetUserById/GetUserByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Features.Users.GetUserById;
+
+public record GetUserByIdQuery(Guid UserId) : IRequest<GetUserByIdResponse>;
---
+++ src/BuildFlow/BuildFlow.Application/Features/Users/GetUserById/GetUserByIdResponse.cs
@@ -0,0 +1,11 @@
+﻿using BuildFlow.Shared.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Application.Features.Users.GetUserById;
+
+public class GetUserByIdResponse : ApiResponse
+{
+    public UserDetailsDto? User { get; set; }
+}
