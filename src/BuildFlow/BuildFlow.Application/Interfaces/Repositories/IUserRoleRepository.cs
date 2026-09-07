fcf587d58c06577d2e03b399c4496c9b08f3346c
Fahad Hasan <hasan.fahad67@outlook.com>
2026-09-07 14:21:01 +0600

    fix some error

--- src/BuildFlow/BuildFlow.Application/Interfaces/Repositories/IUserRoleRepository.cs
+++ src/BuildFlow/BuildFlow.Application/Interfaces/Repositories/IUserRoleRepository.cs
@@ -6,4 +6,6 @@ namespace BuildFlow.Application.Interfaces.Repositories;
 public interface IUserRoleRepository
 {
     Task<Guid> CreateAsync(UserRole userRole, IDbConnection connection, IDbTransaction transaction);
+
+    Task<Role?> GetUserRoleAsync(  Guid userId, Guid tenantId);
 }
